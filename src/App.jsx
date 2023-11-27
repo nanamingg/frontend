f680474557be1ca28a0eb18df4f616457e8ebd22
nanamingg <nana172@kakao.com>
2023-11-27 18:00:36 +0900

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getTodos } from "./redux/appThunk";
+import CreateTodo from "./components/CreateTodo";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  const { todos } = useSelector((state) => state.appReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTodos());
+  }, []);
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="bg-red-100">
+      <CreateTodo />
+    </div>
+  );
 };
 
 export default App;
